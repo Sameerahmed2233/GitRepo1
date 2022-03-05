415282ba5bc40cca564affa78477dde8dd3d9429
nazim <nazim@LAPTOP-KV9GPJ87>
2022-03-05 08:37:02 -0500

    First Commit!!!!

---
+++ src/firstJavaProject/FirstJavaClass.java
@@ -0,0 +1,14 @@
+package firstJavaProject;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		
+		int i = 100;
+		int j = 200;
+		
+		System.out.println(i + j);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitRepo1 {
+}
